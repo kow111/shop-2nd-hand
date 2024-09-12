c22aebb462bfaa7fe1292b3a518b89c4fc5f33b7
Nam <trieunam19955@gmail.com>
2024-09-12 07:39:04 +0700

    aa

--- src/model/user.model.js
+++ src/model/user.model.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   gender: { type: String, enum: ["MALE", "FEMALE", "OTHER"] },
+  is_verified: { type: Boolean, default: false },
   is_admin: { type: Boolean, default: false },
   is_active: { type: Boolean, default: true },
   address: { type: String },
