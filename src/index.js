bf7901b9855a2e3aaa70a76923aa41ca74a12e29
kow111 <chome710@gmail.com>
2024-09-12 07:27:11 +0700

    rename auth router

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ const dotenv = require("dotenv");
 dotenv.config();
 const cors = require("cors");
 const connection = require("./config/database");
-const userRoute = require("./router/user.route");
+const authRoute = require("./router/auth.route");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/v1/user", userRoute);
+app.use("/api/v1/auth", authRoute);
 
 const PORT = process.env.PORT || 3000;
 (async () => {
