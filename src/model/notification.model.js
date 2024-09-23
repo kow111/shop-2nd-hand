f97468dcc102ffa4e553c67062082100d36702c7
kow111 <chome710@gmail.com>
2024-09-23 16:45:09 +0700

    minor fix

--- src/model/notification.model.js
+++ src/model/notification.model.js
@@ -11,21 +11,6 @@ const notificationSchema = new mongoose.Schema({
     ref: "Order",
     required: true,
   },
-  type: {
-    type: String,
-    enum: ["ORDER_STATUS_CHANGE", "NEW_MESSAGE", "PROMOTION"],
-    default: "ORDER_STATUS_CHANGE",
-  },
-  oldStatus: {
-    type: String,
-    enum: ["PENDING", "CONFIRMED", "SHIPPED", "DELIVERED"],
-    required: true,
-  },
-  newStatus: {
-    type: String,
-    enum: ["PENDING", "CONFIRMED", "SHIPPED", "DELIVERED"],
-    required: true,
-  },
   message: {
     type: String,
     default: "",
